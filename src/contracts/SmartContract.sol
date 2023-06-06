ece9cce281c15cc2bcda151cbf000b5d2c391ad8
personal <edgaras.valincius@qa.com>
2023-06-06 16:43:06 +0300

    added test cases and modiefied makefile

--- src/contracts/SmartContract.sol
+++ src/contracts/SmartContract.sol
@@ -1,21 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// Step 1: Import the ERC20 contract
 
-contract SmartContract is ERC20 {
-    address public owner; 
-
-    constructor() ERC20("Fixed", "FIX") {
-        _mint(msg.sender, 1000);
-        owner = msg.sender; 
+// Step 2: Inherit the ERC20 contract
+contract SmartContract {
+    // Step 5: Create the public variable called 'owner'
+    // Step 3: Pass the name and symbol to the ERC20 constructor
+    constructor() {
+        // Step 4: Mint 1000 tokens to msg.sender
+        // Step 6: Set the owner variable to msg.sender
     }
 
-    modifier onlyOwner() { 
-        require(msg.sender == owner, "Only the owner can call this function."); 
-       _; 
-    } 
-
-    function totalSupply() public view override onlyOwner returns (uint256)  {
-        return super.totalSupply();
-    }
+    // Step 7: Create the onlyOwner modifier
+    // Step 8: Require the owner variable to be equal to msg.sender, else throw an error saying "Only the owner can call this function."
+    // Step 9: Override the totalSupply function to make it only accessible to the owner
 }
